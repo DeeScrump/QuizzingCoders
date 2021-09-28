980194ef7dcb6defd1630f2ce19c431db60b6a35
DCKII <dckornbluthii@bellsouth.net>
2021-09-28 00:02:10 -0400

    creating for loops to isolate answers choices for links

--- script.js
+++ script.js
@@ -51,46 +51,78 @@ var counter   = 0;
 function quizStarter() {
   // quizChoices.forEach(function(currentQuestion) {
    
-    // while(counter < quizChoices.length){
-      var ul2 = document.createElement('ul');
-      // var li = document.createElement('li');
-      var h2 = document.createElement('h2');
-      h2.innerHTML = quizChoices[counter].question;
-      var li = document.createElement('li');
-      var div = document.createElement('div');
     
-      var a  = document.createElement('li');
-      a.innerHTML = quizChoices[counter].a;
-      var b  = document.createElement('li');
-      b.innerHTML = quizChoices[counter].b;
-      var c  = document.createElement('li');
-      c.innerHTML = quizChoices[counter].c;
-      var correctAnswer  = document.createElement('li');
-      // correctAnswer.innerHTML = currentQuestion.correctAnswer;
-    
-      a.addEventListener('click',function(){
-        if(counter < quizChoices.length){
+    if(counter < quizChoices.length){
+          var div = document.createElement('div');
+          var ul = document.createElement('ul');
+          var li = document.createElement('li');
+
+          for(var in quizChoices[0]){
+            if(i != 'question' && i != 'correctAnswer' && i != 'userAnswer'){
+              li.
+            }
+          }
           
-        }
+          var ul2 = document.createElement('ul');
+        
+          var h2 = document.createElement('h2');
+          h2.innerHTML = quizChoices[counter].question;
+          var li = document.createElement('li');
+          var div = document.createElement('div');
+        
+          var a  = document.createElement('li');
+          a.innerHTML = quizChoices[counter].a;
+          var b  = document.createElement('li');
+          b.innerHTML = quizChoices[counter].b;
+          var c  = document.createElement('li');
+          c.innerHTML = quizChoices[counter].c;
+          var correctAnswer  = document.createElement('li');
+          
+    
+          
+          ul2.appendChild(a);
+          ul2.appendChild(b);
+          ul2.appendChild(c);
+           
+          div.appendChild(ul2)
+        
+          li.appendChild(h2)
+          li.appendChild(div)
+        
+          questionUl.appendChild(li);
+          // questionUl.appendChild(div)
+          // console.log("its the while loop")
+
+      a.addEventListener('click',function(){
+        counter++;
+        questionUl.innerHTML = '';
+        quizStarter();
       })
+    }else{
+      console.log("HERE")
+    }
     
-      ul2.appendChild(a);
-      ul2.appendChild(b);
-      ul2.appendChild(c);
-      // ul2.appendChild(correctAnswer); 
-      div.appendChild(ul2)
-    
-      li.appendChild(h2)
-      li.appendChild(div)
-    
-      questionUl.appendChild(li);
-      // questionUl.appendChild(div);
-      // console.log("its the while loop")
-    // }
+
 
   
   // })
 }
+
+for(var i in quizChoices[0]){
+  if(i != 'question' && i != 'correctAnswer' && i != 'userAnswer'){
+    console.log(quizChoices[0][i])
+  }
+}
+
+
+// {
+//   "question": "Who invented JavaScript?",
+//     "a": "Douglas Crockford",
+//     "b": "Sheryl Sandberg",
+//     "c": "Brendan Eich",
+//     "correctAnswer": "c",
+//     "userAnswer": null
+// },
   
 startButtonEl.addEventListener('click', clearScreen)
 
